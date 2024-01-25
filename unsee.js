8f9ec338aa935b6da92b82ca140c8db17a3f2aac
Christofer Roth <christofer@dorian.se>
2024-01-25 18:17:50 +0200

    Add bind key to view/unview next

--- unsee.js
+++ unsee.js
@@ -1,3 +1,17 @@
 // Uncheck all review toggles so that they're no longer "viewed"
 // Copy paste into the console
 document.querySelectorAll(".js-reviewed-toggle [type=checkbox]:checked").forEach(c => c.click());
+
+
+// Bind k and K to see and unsee
+(function() {
+  if (window.__bindSeeAndUnsee) return;
+  window.__bindSeeAndUnsee = true;
+  document.addEventListener("keyup", (e) => {
+    if(e.key === 'K') {
+      [...document.querySelectorAll("[name=viewed]:checked")].at(-1)?.click();
+    } else if(e.key === 'k') {
+      document.querySelector("[name=viewed]:not(:checked)")?.click();
+    }
+  });
+})();
